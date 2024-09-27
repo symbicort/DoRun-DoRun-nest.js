c13acb1610f65b8958b60890c729dcdc8fdf3f62
symbicort <jeongwonlee0@gmail.com>
2024-09-27 13:53:47 +0900

    feat: module 생성

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { ChatModule } from './chat/chat.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, ChatModule],
   controllers: [AppController],
   providers: [AppService],
 })
